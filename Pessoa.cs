c2786748d7c67e861901111c3594c59266ac8779
Jonathan Miranda <jmirandamoraes123@gmail.com>
2018-04-17 21:45:25 -0300

    Projeto que pega o arquivo pronto do excel, e organiza ele por nome, sobrenome e idade

---
+++ Pessoa.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Pessoa : IComparable
+    {
+        public string Gender { get; set; }
+        public string GivenName { get; set; }
+        public string MiddleInitial { get; set; }
+        public string SurName { get; set; }
+        public string Age { get; set; }
+
+        public Pessoa (string gender , string givenName, string middleInitial, string sunName, string age)
+        {
+            this.Gender = gender;
+            this.GivenName = givenName;
+            this.MiddleInitial = middleInitial;
+            this.SurName = sunName;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return Gender.PadRight(20)
+                + ""
+                + GivenName.PadRight(20)
+                + ""
+                + MiddleInitial.PadRight(20)
+                + ""
+                + SurName.PadRight(20)
+                + ""
+                + Age;
+
+        }
+
+        public int CompareTo(object obj) {
+            Pessoa outro = (Pessoa)obj;
+            int resultado = GivenName.CompareTo(outro.GivenName);
+            if(resultado != 0) {
+                return resultado;
+            }
+            else {
+                int resultado2 = SurName.CompareTo(outro.SurName);
+                if(resultado2 != 0) {
+                    return resultado2;
+                }
+                else {
+                    return Age.CompareTo(outro.Age);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ClosedXML.Excel;
+using System.Collections.Generic;
+
+namespace ConsoleApp4 {
+    class Program {
+        public static object Provider { get; private set; }
+
+        static void Main(string[] args) {
+            var wb = new XLWorkbook(@"pasta1.xlsx");
+            var planilha = wb.Worksheet(1);
+
+            List<Pessoa> plist = new List<Pessoa>();
+            Pessoa pessoa;
+
+            Console.WriteLine("".PadRight('-'));
+            Console.WriteLine("Gender".PadRight(20) + "GivenName".PadRight(20) + "MiddleInitial".PadLeft(10) + "SurName".PadLeft(14) + "Age".PadLeft(16));
+            Console.WriteLine("".PadRight(100, '-'));
+            var linha = 2;
+            while (true) {
+                var gender = planilha.Cell("A" + linha.ToString()).Value.ToString();
+                var givename = planilha.Cell("B" + linha.ToString()).Value.ToString();
+                var middleinital = planilha.Cell("C" + linha.ToString()).Value.ToString();
+                var surame = planilha.Cell("D" + linha.ToString()).Value.ToString();
+                var age = planilha.Cell("E" + linha.ToString()).Value.ToString();
+                if (string.IsNullOrEmpty(givename)) break;
+
+                pessoa = new Pessoa(gender, givename, middleinital, surame, age);
+                plist.Add(pessoa);
+
+                linha++;
+            }
+            plist.Sort();
+
+
+            foreach (Pessoa A in plist) {
+                Console.WriteLine(A);
+            }
+
+
+
+            planilha.Dispose();
+            wb.Dispose();
+            Console.WriteLine("".PadRight(100, '-'));
+            Console.WriteLine("By Jonathan Miranda de Moraes");
+            Console.ReadKey();
+
+        }
+    }
+}
